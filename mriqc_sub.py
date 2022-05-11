bbf9d8efefd1e7f9a8b804f0f49ede9a659733e6
MarsMellow <mpzwiers@gmail.com>
2022-05-11 17:12:14 +0200

    Added a `nosub` option to run mriqc on the local host

--- mriqc_sub.py
+++ mriqc_sub.py
@@ -15,7 +15,7 @@ import subprocess
 from pathlib import Path
 
 
-def main(bidsdir: str, outputdir: str, workdir_: str, sessions=(), force=False, mem_gb=18, walltime=8, file_gb_=50, argstr='', qargstr='', dryrun=False, skip=True):
+def main(bidsdir: str, outputdir: str, workdir_: str, sessions=(), force=False, mem_gb=18, walltime=8, file_gb_=50, args='', qargs='', dryrun=False, nosub=False, skip=True):
 
     # Default
     bidsdir   = Path(bidsdir)
@@ -76,27 +76,18 @@ def main(bidsdir: str, outputdir: str, workdir_: str, sessions=(), force=False, 
                     if not dryrun:
                         report.unlink()
 
-            command = """qsub -l walltime={walltime}:00:00,mem={mem_gb}gb{file_gb} -N mriqc_sub-{sub_id}_{ses_id} {qargs} <<EOF
-                         cd {pwd}
-                         {mriqc} {bidsdir} {outputdir} participant -w {workdir} --participant-label {sub_id} {ses_id_opt} --verbose-reports --mem_gb {mem_gb} --ants-nthreads 1 --nprocs 1 {args}\nEOF"""\
-                         .format(pwd        = Path.cwd(),
-                                 mriqc      = f'unset PYTHONPATH; export PYTHONNOUSERSITE=1; singularity run --cleanenv {os.getenv("DCCN_OPT_DIR")}/mriqc/{os.getenv("MRIQC_VERSION")}/mriqc-{os.getenv("MRIQC_VERSION")}.simg',
-                                 bidsdir    = bidsdir,
-                                 outputdir  = outputdir,
-                                 workdir    = workdir,
-                                 sub_id     = sub_id[4:],
-                                 ses_id     = ses_id,
-                                 ses_id_opt = ses_id_opt,
-                                 mem_gb     = mem_gb,
-                                 walltime   = walltime,
-                                 file_gb    = file_gb,
-                                 args       = argstr,
-                                 qargs      = qargstr)
+            qsub  = f"qsub -l walltime={walltime}:00:00,mem={mem_gb}gb{file_gb} -N mriqc_{sub_id}_{ses_id} {qargs}"
+            mriqc = f'unset PYTHONPATH; export PYTHONNOUSERSITE=1; singularity run --cleanenv {os.getenv("DCCN_OPT_DIR")}/mriqc/{os.getenv("MRIQC_VERSION")}/mriqc-{os.getenv("MRIQC_VERSION")}.simg {bidsdir} {outputdir} participant -w {workdir} --participant-label {sub_id[4:]} {ses_id_opt} --verbose-reports --mem_gb {mem_gb} --ants-nthreads 1 --nprocs 1 {args}'
+            if nosub:
+                command = f"cd {Path.cwd()}\n{mriqc}"
+            else:
+                command = f"{qsub} <<EOF\ncd {Path.cwd()}\n{mriqc}\nEOF"
+
             running = subprocess.run('if [ ! -z "$(qselect -s RQH)" ]; then qstat -f $(qselect -s RQH) | grep Job_Name | grep mriqc_sub-; fi', stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
             if skip and f"mriqc_{sub_id}_{ses_id}" in running.stdout.decode():
                 print(f"--> Skipping already running / scheduled job ({n+1}/{len(sessions)}): mriqc_{sub_id}_{ses_id}")
             else:
-                print(f"--> Submitting job ({n+1}/{len(sessions)}):\n{command}")
+                print(f"--> {'Running' if nosub else 'Submitting'} job ({n+1}/{len(sessions)}):\n{command}")
                 if not dryrun:
                     process = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
                     if process.stderr.decode('utf-8') or process.returncode!=0:
@@ -144,6 +135,7 @@ if __name__ == "__main__":
     parser.add_argument('-l','--local_gb',  help='Required free diskspace of the local temporary workdir (in gb)', default=50, type=int)
     parser.add_argument('-a','--args',      help='Additional arguments that are passed to mriqc (NB: Use quotes and a leading space to prevent unintended argument parsing)', type=str, default='')
     parser.add_argument('-q','--qargs',     help='Additional arguments that are passed to qsub (NB: Use quotes and a leading space to prevent unintended argument parsing)', type=str, default='')
+    parser.add_argument('-n','--nosub',     help='Add this flag to run the mriqc commands locally, without submitting them (useful for debugging)', action='store_true')
     parser.add_argument('-d','--dryrun',    help='Add this flag to just print the mriqc qsub commands without actually submitting them (useful for debugging)', action='store_true')
     args = parser.parse_args()
 
@@ -155,7 +147,8 @@ if __name__ == "__main__":
          mem_gb    = args.mem_gb,
          walltime  = args.time,
          file_gb_  = args.local_gb,
-         argstr    = args.args,
-         qargstr   = args.qargs,
+         args      = args.args,
+         qargs     = args.qargs,
          dryrun    = args.dryrun,
+         nosub     = args.nosub,
          skip      = args.ignore)
